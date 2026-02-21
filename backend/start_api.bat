d5f18fb9d6c8f9ea0735c8c95c91a810c74e8738
Hugues Gauthier <hugues.gauthier@gmail.com>
2026-02-21 15:45:01 -0500

    fix

--- backend/start_api.bat
+++ backend/start_api.bat
@@ -6,5 +6,5 @@
 REM Force Python to flush stdout/stderr immediately
 set PYTHONUNBUFFERED=1
 
-".\venv\Scripts\uvicorn.exe" app.main:app --reload
+".\venv\Scripts\python.exe" -m uvicorn app.main:app --reload
 pause
